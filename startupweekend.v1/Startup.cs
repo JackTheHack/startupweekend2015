2fcb069a4822de5338f11222ad729a998a5e6a68
jack.spektor <jack.spektor@gmail.com>
2015-11-14 10:59:01 +1300

    Initial commit

---
+++ startupweekend.v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(startupweekend.v1.Startup))]
+namespace startupweekend.v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
